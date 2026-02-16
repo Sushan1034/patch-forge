38b1a0988a8a41e1285e0a3bcc1c409ab9eea6a9
Sushan Aryal <125583671+Sushan1034@users.noreply.github.com>
2026-02-16 23:23:16 +0545

    change_v1_finpos_id
    Changed id column type from INT to SERIAL.

--- finpos/v1/20260216_11020_create_books.sql
+++ finpos/v1/20260216_11020_create_books.sql
@@ -2,7 +2,7 @@
 -- PATCH_TYPE: SCHEMA
 
 CREATE TABLE books (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     author VARCHAR(255) NOT NULL,
     published_date DATE,
