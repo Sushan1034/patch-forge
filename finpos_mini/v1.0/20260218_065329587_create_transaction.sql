4f340da4c411c2a614d35de87955cb04b9998f4b
uniquenepal <161020377+uniquenepal@users.noreply.github.com>
2026-02-18 12:44:13 +0545

    finpos_mini

---
+++ finpos_mini/v1.0/20260218_065329587_create_transaction.sql
@@ -0,0 +1,12 @@
+-- PATCH-ID=20260218_065329587_create_transaction
+-- PATCH_TYPE=MIXED
+-- AUTHOR: Sushan Aryal
+
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,   
+    amount DECIMAL(10, 2) NOT NULL,
+    transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
+
+INSERT INTO transactions (id, amount) VALUES (1, 100.00);
+INSERT INTO transactions (id, amount) VALUES (2, 150.50);
