b48ea7973cdadb320ebba311927c620276076aa6
uniquenepal <161020377+uniquenepal@users.noreply.github.com>
2026-02-18 13:11:07 +0545

    Insert the data

---
+++ finpos_mini/v2.0/20260218_072445778_insert_transaction.sql
@@ -0,0 +1,5 @@
+-- PATCH_ID: 20260218_072445778_insert_transaction.sql
+-- PATCH_TYPE: DATA
+
+INSERT INTO transactions (id, amount) VALUES (3, 200.75);
+INSERT INTO transactions (id, amount) VALUES (4, 300.25);
