d6050cfd1f170ff87aa26afe0598d11521016c92
Sushan Aryal <125583671+Sushan1034@users.noreply.github.com>
2026-02-18 14:53:30 +0545

    Insert new transaction records
    Added new transaction records with IDs 3 and 4.

--- finpos_mini/v2.0/20260218_072445778_insert_transaction.sql
+++ finpos_mini/v2.0/20260218_072445778_insert_transaction.sql
@@ -1,5 +1,6 @@
 -- PATCH_ID: 20260218_072445778_insert_transaction.sql
 -- PATCH_TYPE: DATA
+-- AUTHOR: Manish Karki
 
 INSERT INTO transactions (id, amount) VALUES (3, 200.75);
 INSERT INTO transactions (id, amount) VALUES (4, 300.25);
