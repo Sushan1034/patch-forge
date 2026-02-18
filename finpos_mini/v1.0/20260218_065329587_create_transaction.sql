427d73606722c42d79610d384f118daae072d3ec
Sushan Aryal <125583671+Sushan1034@users.noreply.github.com>
2026-02-18 13:17:03 +0545

    Update patch metadata in create_transaction.sql
    Updated patch metadata comments in the SQL file.

--- finpos_mini/v1.0/20260218_065329587_create_transaction.sql
+++ finpos_mini/v1.0/20260218_065329587_create_transaction.sql
@@ -1,5 +1,5 @@
--- PATCH_ID=20260218_065329587_create_transaction
--- PATCH_TYPE=MIXED
+-- PATCH_ID: 20260218_065329587_create_transaction
+-- PATCH_TYPE: MIXED
 -- AUTHOR: Sushan Aryal
 
 CREATE TABLE transactions (
