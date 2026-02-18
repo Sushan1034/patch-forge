226cecff1b8d8b45971776973435ed8ab62b7060
uniquenepal <161020377+uniquenepal@users.noreply.github.com>
2026-02-18 13:00:29 +0545

    V2_finpos_mini

---
+++ finpos_mini/v2.0/createtransaction.sql
@@ -0,0 +1,9 @@
+-- PATCH_ID: 20260218_071411311_create_transaction.sql
+-- PATCH_TYPE: SCHEMA
+-- AUTHOR: Sushan Aryal
+
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,   
+    amount DECIMAL(10, 2) NOT NULL,
+    transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
