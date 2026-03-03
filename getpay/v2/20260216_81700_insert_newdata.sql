f10e8b9e8d03e6ac42a48abee72dfb6318983b69
Sushan Aryal <125583671+Sushan1034@users.noreply.github.com>
2026-03-03 13:33:17 +0545

    Author added

--- getpay/v2/20260216_81700_insert_newdata.sql
+++ getpay/v2/20260216_81700_insert_newdata.sql
@@ -1,6 +1,6 @@
 -- PATCH_ID: 20260216_81700_insert_newdata
 -- PATCH_TYPE: DATA
-
+-- AUTHOR: Sushan Aryal
 INSERT INTO users (name, phone) VALUES ('Eve', 1234567890);
 INSERT INTO users (name, phone) VALUES ('Frank', 9876543210);
 INSERT INTO users (name, phone) VALUES ('Grace', 5555555555);
