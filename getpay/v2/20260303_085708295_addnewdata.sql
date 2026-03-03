7dcc9b2a400b44253f78574b54dc03566ca243d1
Sushan Aryal <125583671+Sushan1034@users.noreply.github.com>
2026-03-03 14:45:04 +0545

    getpay_v2_update_patch
    Add new user data to the users table.

---
+++ getpay/v2/20260303_085708295_addnewdata.sql
@@ -0,0 +1,6 @@
+-- PATCH_ID: 20260303_085708295_addnewdata.sql
+-- PATCH_TYPE: DATA
+-- AUTHOR: Balen Shah
+
+INSERT INTO users (name) VALUES ('Sushan');
+INSERT INTO users (name) VALUES ('Aryal');  
