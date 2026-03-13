162a3febcfc480bb9e2d6a317af1fbdce851c773
Sushan Aryal <125583671+Sushan1034@users.noreply.github.com>
2026-03-13 10:41:06 +0545

    Update author for Pride and Prejudice and add Jay Nepal

--- finpos/v1/20260216_11060_insert_books.sql
+++ finpos/v1/20260216_11060_insert_books.sql
@@ -4,4 +4,6 @@
 INSERT INTO books (title, author, published_date, isbn, price) VALUES ('The Great Gatsby', 'F. Scott Fitzgerald', '1925-04-10', '978-0743273565', 10.99);
 INSERT INTO books (title, author, published_date, isbn, price) VALUES ('To Kill a Mockingbird', 'Harper Lee', '1960-07-11', '978-0061120084', 7.99);
 INSERT INTO books (title, author, published_date, isbn, price) VALUES ('1984    ', 'George Orwell', '1949-06-08', '978-0451524935', 8.99);
-INSERT INTO books (title, author, published_date, isbn, price) VALUES ('Pride and Prejudice', 'Jane Austen', '1813-01-28', '978-1503290563', 6.99);
+INSERT INTO books (title, author, published_date, isbn, price) VALUES ('Pride and Prejudice', 'Sushan Aryal', '1813-01-28', '978-1503290563', 6.99);
+INSERT INTO books (title, author, published_date, isbn, price) VALUES ('Jay Nepal', 'Jane Aryal', '1814-01-28', '979-1503290563', 7.99);
+
