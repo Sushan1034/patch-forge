6c0ae48b6874b5eadb6d551a7d38ab53658b93f9
Sushan Aryal <125583671+Sushan1034@users.noreply.github.com>
2026-04-02 13:43:32 +0545

    Add new book entries to the database
    Inserted new book records into the books table.

---
+++ finpos/v1/20260402_075735118_insertdata.sql
@@ -0,0 +1,6 @@
+-- PATCH_ID: 20260402_075735118_insertdata.sql
+-- PATCH_TYPE: DATA
+-- AUTHOR: Sushan Aryal
+
+INSERT INTO books (title, author, published_date, isbn, price) VALUES ('The Book Thieff', 'Aarkus Zusak', '2005-03-14', '978-0375842207', 9.95);
+INSERT INTO books (title, author, published_date, isbn, price) VALUES ('Life of Pie', 'Yann Gartel', '2001-09-11', '978-0156027328', 10.20);
